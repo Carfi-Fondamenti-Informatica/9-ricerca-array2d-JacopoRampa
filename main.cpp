73c6ac568809e921dc074d5d0e12a8affec1ef50
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-12-07 14:28:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    char lista[10][20];
+    for (int i=0; i<10; i++) {
+        for (int j=0; j<20; j++) {
+            lista[i][j]=' ';
+        }
+    }
+    char nome[20];
+    for (int i=0; i<20; i++) {
+        nome[i]=' ';
+    }
+    for (int i=0; i<10; i++) {
+        cin >> lista[i];
+    }
+        cin >> nome;
+    if (cerca_nome(lista,nome)<=10){
+        cout << cerca_nome(lista,nome);
+    }else{
+        cout << "non presente" << endl;
+    }
+    return 0;
 }
