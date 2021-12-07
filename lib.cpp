158fdbf00197c7260c1a1aa2823b4a498b8ccd78
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-12-07 14:28:25 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "lib.h"
+int cerca_nome(char lista[10][20], char nome[20]) {
+    for (int i=0; i<10; i++) {
+        for (int j=0; j<20; j++) {
+            if (lista[i][j]!=nome[j]) {
+                j = 20;
+            }else if(j==19){
+                return i;
+            }
+        }
+    }
+    return 11;
+}
